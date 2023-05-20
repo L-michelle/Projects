d87d2f5f64f6d4624f332fcfd968da9b13c02c62
Michelle L <111924544+L-michelle@users.noreply.github.com>
2023-05-20 11:54:18 -0700

    Update rideshare_analysis.sql
    added line spacing to sep queries

--- Rideshare Analysis (SQL)/rideshare_analysis.sql
+++ Rideshare Analysis (SQL)/rideshare_analysis.sql
@@ -27,6 +27,7 @@ weather_records = pd.DataFrame(content, columns=heading_table)
 weather_records
 
 
+
 -- using SQLite Studio
 /*Print the company_name field. Find the number of taxi rides for each taxi company for November 15-16, 2017, name the resulting field trips_amount, and print it, too. Sort the results by the trips_amount field in descending order.*/
 select 
@@ -40,6 +41,9 @@ group by
     company_name
 order by
     trips_amount desc; 
+    
+    
+    
     
 /* 
 Find the number of rides for every taxi company whose name contains the words "Yellow" or "Blue" for November 1-7, 2017. Name the resulting variable trips_amount. Group the results by the company_name field. */
@@ -70,6 +74,8 @@ from
 as SUBQ
 
 
+
+
 /*For November 1-7, 2017, the most popular taxi companies were Flash Cab and Taxi Affiliation Services. Find the number of rides for these two companies and name the resulting variable trips_amount. Join the rides for all other companies in the group "Other." Group the data by taxi company names. Name the field with taxi company names company. Sort the result in descending order by trips_amount. */
 select
     case 
@@ -88,6 +94,8 @@ order by
     trips_amount desc; 
     
     
+    
+    
 -- Retrieve the identifiers of the O'Hare and Loop neighborhoods from the neighborhoods table.--
 select
     neighborhood_id,
@@ -97,7 +105,9 @@ from
 where 
     name like '%Hare' 
     OR name LIKE 'Loop'
-    
+
+
+
 /* For each hour, retrieve the weather condition records from the weather_records table. Using the CASE operator, break all hours into two groups: Bad if the description field contains the words rain or storm, and Good for others. Name the resulting field weather_conditions. The final table must include two fields: date and hour (ts) and weather_conditions. */
 select
     date_trunc('hour', ts) as ts,
@@ -106,6 +116,8 @@ select
     else 'Good' End as weather_conditions
 from
     weather_records
+    
+    
     
     
 /* Retrieve from the trips table all the rides that started in the Loop (pickup_location_id: 50) on a Saturday and ended at O'Hare (dropoff_location_id: 63). Get the weather conditions for each ride. Use the method you applied in the previous task. Also, retrieve the duration of each ride. Ignore rides for which data on weather conditions is not available.
