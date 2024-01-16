e50e5e3f47da544ad449745ee375ca878b0c6632
Michelle L <111924544+L-michelle@users.noreply.github.com>
2024-01-16 11:09:18 -0800

    Update rideshare_analysis.sql

--- Rideshare Analysis (SQL)/rideshare_analysis.sql
+++ Rideshare Analysis (SQL)/rideshare_analysis.sql
@@ -29,7 +29,8 @@ weather_records
 
 
 -- using SQLite Studio
-/*Print the company_name field. Find the number of taxi rides for each taxi company for November 15-16, 2017, name the resulting field trips_amount, and print it, too. Sort the results by the trips_amount field in descending order.*/
+/*Print the company_name field. Find the number of taxi rides for each taxi company for November 15-16, 2017, 
+name the resulting field trips_amount, and print it, too. Sort the results by the trips_amount field in descending order.*/
 select 
     cabs.company_name as company_name,
     count(trips.trip_id) as trips_amount
@@ -46,7 +47,8 @@ order by
     
     
 /* 
-Find the number of rides for every taxi company whose name contains the words "Yellow" or "Blue" for November 1-7, 2017. Name the resulting variable trips_amount. Group the results by the company_name field. */
+Find the number of rides for every taxi company whose name contains the words "Yellow" or "Blue" for November 1-7, 2017. 
+Name the resulting variable trips_amount. Group the results by the company_name field. */
 select
     *
 from
@@ -76,7 +78,10 @@ as SUBQ
 
 
 
-/*For November 1-7, 2017, the most popular taxi companies were Flash Cab and Taxi Affiliation Services. Find the number of rides for these two companies and name the resulting variable trips_amount. Join the rides for all other companies in the group "Other." Group the data by taxi company names. Name the field with taxi company names company. Sort the result in descending order by trips_amount. */
+/*For November 1-7, 2017, the most popular taxi companies were Flash Cab and Taxi Affiliation Services. 
+Find the number of rides for these two companies and name the resulting variable trips_amount. 
+Join the rides for all other companies in the group "Other." Group the data by taxi company names. 
+Name the field with taxi company names company. Sort the result in descending order by trips_amount. */
 select
     case 
     when
@@ -108,7 +113,10 @@ where
 
 
 
-/* For each hour, retrieve the weather condition records from the weather_records table. Using the CASE operator, break all hours into two groups: Bad if the description field contains the words rain or storm, and Good for others. Name the resulting field weather_conditions. The final table must include two fields: date and hour (ts) and weather_conditions. */
+/* For each hour, retrieve the weather condition records from the weather_records table. 
+Using the CASE operator, break all hours into two groups: Bad if the description field contains the words rain or storm, 
+and Good for others. Name the resulting field weather_conditions. 
+The final table must include two fields: date and hour (ts) and weather_conditions. */
 select
     date_trunc('hour', ts) as ts,
     case
@@ -120,7 +128,9 @@ from
     
     
     
-/* Retrieve from the trips table all the rides that started in the Loop (pickup_location_id: 50) on a Saturday and ended at O'Hare (dropoff_location_id: 63). Get the weather conditions for each ride. Use the method you applied in the previous task. Also, retrieve the duration of each ride. Ignore rides for which data on weather conditions is not available.
+/* Retrieve from the trips table all the rides that started in the Loop (pickup_location_id: 50) on a Saturday and ended at O'Hare 
+(dropoff_location_id: 63). Get the weather conditions for each ride. Use the method you applied in the previous task. 
+Also, retrieve the duration of each ride. Ignore rides for which data on weather conditions is not available.
 The table columns should be in the following order:
 - start_ts
 - weather_conditions
